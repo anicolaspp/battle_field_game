ecf4157ef9274f60182d32e3700d72bf5fe620f9
Nicolas A Perez <anicolaspp@gmail.com>
2014-06-13 16:09:05 -0400

    implementing strategies

---
+++ Final_Project_P3/RandomMoveStrategy.c
@@ -0,0 +1,28 @@
+//
+//  RandomMoveStrategy.c
+//  Final_Project_P3
+//
+//  Created by Nicolas A Perez on 6/13/14.
+//  Copyright (c) 2014 Nicolas A Perez. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+#include "RandomMoveStrategy.h"
+
+
+
+int RandomMoveStrategy_Play(Vector vector)
+{
+	if (vector.count > 0)
+	{
+		// Need a random position here
+		
+		int position = 0;
+		
+		return position;
+	}
+	
+	return -1;
+}
---
+++ Final_Project_P3/SingleMoveStrategy.c
@@ -0,0 +1,22 @@
+//
+//  SingleMoveStrategy.c
+//  Final_Project_P3
+//
+//  Created by Nicolas A Perez on 6/13/14.
+//  Copyright (c) 2014 Nicolas A Perez. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "SingleMoveStrategy.h"
+
+
+int SingleMoveStrategy_Play(Vector vector)
+{
+	if (vector.count > 0)
+	{
+		return vector.values[0];
+	}
+	
+	return -1;
+}
