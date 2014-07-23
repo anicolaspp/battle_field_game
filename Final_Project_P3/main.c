253726d999adc269f28adca2af0782936a302b6f
Nicolas A Perez <anicolaspp@gmail.com>
2014-07-22 23:43:30 -0400

    rafactoring

--- Final_Project_P3/main.c
+++ Final_Project_P3/main.c
@@ -44,30 +44,25 @@ void CreatePipes(int numberOfPipes);
 void UnBind(int pipesCount);
 
 int IsInputPipeIndex(int index);
-
-
+void PrintStats(Table *table, Vector *emptyCellVector);
+void ProcessInputFileDecriptors(struct pollfd * fds);
+void InitPlayerIds(int numberOfGames);
 
 
 int main(int argc, const char * argv[])
 {
-		//if (argc == 2)
+	if (argc == 2)
 	{
 		signal(SIGTERM, sigterm_handler);
 		
 		pid = getpid();
 		printf("%d\n", pid);
 		
-		numberOfGames = 1; //atoi(argv[1]);
-		
-		playerIds = malloc(sizeof(int) * numberOfGames);
-		
-		for (int i = 0 ; i < numberOfGames; i++)
-		{
-			playerIds[i] = -1;
-		}
-		
+		numberOfGames = atoi(argv[1]);
 		numberOfPipes = numberOfGames * 2;
+
 		CreatePipes(numberOfPipes);
+		InitPlayerIds(numberOfGames);
 		
 		while (true)
 		{
@@ -75,32 +70,7 @@ int main(int argc, const char * argv[])
 			
 			if (ret > 0)
 			{
-				for (int i = 0; i < numberOfPipes; i++)
-				{
-					if (fds[i].revents == POLLIN)
-					{
-						assert(IsInputPipeIndex(i));
-						
-						ProcessFile(fds[i].fd, fds[i + 1].fd, i);
-//						
-//						pid_t id = fork();
-//						
-//												
-//						if (id == 0)
-//						{
-//							printf("%d\n", getpid());
-//							
-//							int x = dup(fds[i].fd);
-//							int y = dup(fds[i + 1].fd);
-//							
-//							ProcessFile(x, y);
-//
-//								//							ProcessFile(fds[i].fd, fds[i + 1].fd);
-//							
-//							return 0;
-//						}
-					}
-				}
+				ProcessInputFileDecriptors(fds);
 			}
 		}
 	}
@@ -108,36 +78,80 @@ int main(int argc, const char * argv[])
     return 0;	
 }
 
+void ProcessInputFileDecriptors(struct pollfd * fds)
+{
+	for (int i = 0; i < numberOfPipes; i++)
+	{
+		if (fds[i].revents == POLLIN)
+		{
+			assert(IsInputPipeIndex(i));
+			
+			ProcessFile(fds[i].fd, fds[i + 1].fd, i);
+		}
+	}
+}
+
+void InitPlayerIds(int numberOfGames)
+{
+	playerIds = malloc(sizeof(int) * numberOfGames);
+	
+	for (int i = 0 ; i < numberOfGames; i++)
+	{
+		playerIds[i] = -1;
+	}
+}
+
 void sigterm_handler()
 {
 	UnBind(numberOfPipes);
 	
 	free(playerIds);
-	
-		//TODO: I will probably have to wait for children to finish before exit
-	
+
 	exit(EXIT_SUCCESS);
 }
 
-
-
-int IsInputPipeIndex(int index)
+void ProcessFile(int fd, int outFd, int gameId)
 {
-	return index % 2 == 0;
+	Table *table = ReadTableFromFile(fd);
+	
+	if (table != NULL)
+	{
+		Vector * emptyCellVector = GetXVector();
+		
+		PrintStats(table, emptyCellVector);
+		
+		int playerId = GetPlayerId(gameId, table);
+		
+		TPoint move = RandomMoveStrategy_Play(emptyCellVector);
+		
+		char buffer[100];
+		int len = sprintf(buffer, "[%d, %d, %d]\n", playerId, move.X + 1, move.Y + 1);
+		
+		write(outFd, buffer, len);
+		
+		free(emptyCellVector->values);
+		free(emptyCellVector);
+		
+		free(table->grid);
+		free(table);
+	}
 }
 
-void ProcessFileContentAsync(int fd, int outFd, int gameId)
+void PrintStats(Table *table, Vector *emptyCellVector)
 {
-	ProcessFile(fd, outFd, gameId);
-		//
-		//
-		//	pid_t nPid = fork();
-		//
-		//	if (nPid)
-		//	{
-		//		ProcessFile(fd, outputFileName);
-		//		exit(EXIT_SUCCESS);
-		//	}
+	printf("Size: %d\n", table->dimensions);
+	printf("Number of players: %d\n", table->numberOfPlayers);
+	
+	printf("Number of empty cells: %d\n", emptyCellVector->count);
+	
+	if (emptyCellVector->count > 0)
+	{
+		printf("Location of first empty cells: [%d, %d]\n",
+			   emptyCellVector->values[0].X + 1, emptyCellVector->values[0].Y + 1);
+		
+		printf("Location of last empty cells: [%d, %d]\n",
+			   emptyCellVector->values[emptyCellVector->count - 1].X + 1, emptyCellVector->values[emptyCellVector->count - 1].Y + 1);
+	}
 }
 
 int GetPlayerId(int gameId, Table * table)
@@ -167,43 +181,28 @@ int GetPlayerId(int gameId, Table * table)
 	return maxValue + 1;
 }
 
-void ProcessFile(int fd, int outFd, int gameId)
+void CreatePipes(int numberOfPipes)
 {
-	Table *table = ReadTableFromFile(fd);
+	fds = malloc(sizeof(struct pollfd) * numberOfPipes);
 	
-	if (table != NULL)
+	for (int i = 0; i < numberOfPipes; i += 2)
 	{
-		Vector * emptyCellVector = GetXVector();
+		char * input = GetInputFileName(i);
 		
-		printf("Size: %d\n", table->dimensions);
-		printf("Number of players: %d\n", table->numberOfPlayers);
+		mkfifo(input, 0777);
 		
-		printf("Number of empty cells: %d\n", emptyCellVector->count);
+		fds[i].fd = open(input, O_RDWR);
+		fds[i].events = POLLIN;
+		fds[i].revents = 0;
 		
-		if (emptyCellVector->count > 0)
-		{
-			printf("Location of first empty cells: [%d, %d]\n",
-				   emptyCellVector->values[0].X + 1, emptyCellVector->values[0].Y + 1);
-			
-			printf("Location of last empty cells: [%d, %d]\n",
-				   emptyCellVector->values[emptyCellVector->count - 1].X + 1, emptyCellVector->values[emptyCellVector->count - 1].Y + 1);
-		}
+		char * output = GetOutputFileName(i);
 		
-		int playerId = GetPlayerId(gameId, table);
+		mkfifo(output, 0777);
 		
+		fds[i + 1].fd = open(output, O_RDWR);
 		
-		TPoint move = RandomMoveStrategy_Play(emptyCellVector);
-		
-		char buffer[100];
-		int len = sprintf(buffer, "[%d, %d, %d]\n", playerId, move.X + 1, move.Y + 1);
-		
-		write(outFd, buffer, len);
-			
-		free(emptyCellVector->values);
-		free(emptyCellVector);
-		
-		free(table->grid);
-		free(table);
+		free(input);
+		free(output);
 	}
 }
 
@@ -227,32 +226,9 @@ void UnBind(int pipesCount)
 	free(fds);
 }
 
-
-
-
-void CreatePipes(int numberOfPipes)
+int IsInputPipeIndex(int index)
 {
-	fds = malloc(sizeof(struct pollfd) * numberOfPipes);
-	
-	for (int i = 0; i < numberOfPipes; i += 2)
-	{
-		char * input = GetInputFileName(i);
-		
-		mkfifo(input, 0600);
-		
-		fds[i].fd = open(input, O_RDWR);
-		fds[i].events = POLLIN;
-		fds[i].revents = 0;
-		
-		char * output = GetOutputFileName(i);
-		
-		mkfifo(output, 0600);
-		
-		fds[i + 1].fd = open(output, O_RDWR);
-		
-		free(input);
-		free(output);
-	}
+	return index % 2 == 0;
 }
 
 char * GetOutputFileName(int index)
